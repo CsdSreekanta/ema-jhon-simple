94572d41b3ed7a64f33efbc9b73d81ecf6e27618
CsdSreekanta <sreekanta.csd@gmail.com>
2022-04-09 09:33:17 +0600

    product details added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,11 +2,19 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-   const {name, seller, price, rating, img} =props.product
+   const {name, seller, price, ratings, img} =props.product
     return (
         <div className='product'>
            <img src={img} alt="" />
-           <p className='product-name'>{name}</p>
+           <div className='product-details'>
+                <p className='product-name'>{name}</p>
+                <p>Price:${price}</p>
+                <p><small>Manufacturer:{seller}</small></p>
+                <p><small>Rating:{ratings}</small></p>
+           </div>
+           <button className='button-cart'>
+               <p>Add to Card</p>
+           </button>
         </div>
     );
 };
